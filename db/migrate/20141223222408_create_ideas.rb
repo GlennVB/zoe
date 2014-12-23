bd72b94cd4530131cd84fd2a190bd6169ead29c8
Glenn Van Bael <glenn@digital-ad.be>
2014-12-24 00:00:12 +0100

    Idea description migration bug fix

--- db/migrate/20141223222408_create_ideas.rb
+++ db/migrate/20141223222408_create_ideas.rb
@@ -2,7 +2,7 @@ class CreateIdeas < ActiveRecord::Migration
   def change
     create_table :ideas do |t|
       t.string :name, default: ""
-      t.text :description, default: ""
+      t.text :description
       t.boolean :completed, default: false
       t.integer :user_id
       t.boolean :published, default: false
