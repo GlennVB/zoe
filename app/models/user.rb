ce14f0102654e3b2a5aaad87230c5ab18a963e1c
Glenn Van Bael <glenn@digital-ad.be>
2015-01-15 16:16:22 +0100

    Update timeoutable

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,7 +33,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,:confirmable, :lockable, :timeoutable
+         :recoverable, :rememberable, :trackable, :validatable,:confirmable, :lockable
   has_many :api_keys
   has_many :user_updates
   has_many :ideas
